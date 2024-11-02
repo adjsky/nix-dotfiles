7ece4471f85dbaa124034eca47c7c5cdaea41a61
adjsky <adjsky@adjsky-macbook.local>
2024-11-02 15:24:48 +0300

    setup autorebase/merge

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,8 +1,11 @@
 {
   enable = true;
   extraConfig = {
+    pull.default = "current";
     pull.rebase = true;
     fetch.prune = true;
+    branch.autoSetupMerge = "always";
+    branch.autoSetupRebase = "always";
     diff.colorMoved = "zebra";
     push.default = "current";
     core.editor = "nvim";
