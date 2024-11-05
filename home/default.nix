22c1b13fece7dee73fcb9eeaad33e2af683d3a52
adjsky <adjsky@adjsky-macbook.local>
2024-11-06 00:52:00 +0300

    some zed setup

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
-  # internal compatibility configuration for home-manager, don't change this!
+  # Internal compatibility configuration for home-manager, don't change this!
   home.stateVersion = "23.05";
 
   home.packages = with pkgs; [
@@ -19,15 +19,18 @@
     rebar3
     tableplus
     colima
+    zed-editor
   ];
+
+  # Use the external zed config for quicker hacking.
+  xdg.configFile."zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nix-dotfiles/home/config/zed/settings.json";
 
   # Let home-manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.zsh = import ./programs/zsh.nix;
+  programs.zsh = import ./programs/zsh.nix { inherit config; };
   programs.git = import ./programs/git.nix;
   programs.wezterm = import ./programs/wezterm.nix;
   programs.neovim = import ./programs/nvim.nix { inherit pkgs; };
   programs.ssh = import ./programs/ssh.nix;
-  programs.zed-editor = import ./programs/zed.nix;
 }
--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -1,9 +1,10 @@
+{ config }:
 {
   enable = true;
   autosuggestion.enable = true;
   syntaxHighlighting.enable = true;
   shellAliases = {
-    switch = "darwin-rebuild switch --flake path:$HOME/.config/nix-dotfiles";
+    switch = "darwin-rebuild switch --flake path:${config.xdg.configHome}/nix-dotfiles";
   };
   oh-my-zsh = {
     enable = true;
