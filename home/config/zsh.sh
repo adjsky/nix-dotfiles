09869c6aa79dc1a7296d556c7ab46e747e17eafd
adjsky <adjsky@adjsky-macbook.local>
2026-02-07 22:01:24 +0300

    wrap in quotes

--- home/config/zsh.sh
+++ home/config/zsh.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Applications/Android Studio.app/Contents/jbr/Contents/Home
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
 
 function kp { kill $(lsof -t -i:$1); }
 
