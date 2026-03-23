2f1b7e5d7c12a3ea82ff80d6766bbf0b8365e2fd
adjsky <adjsky@adjsky-macbook.local>
2026-03-23 14:38:11 +0300

    fix

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -2,7 +2,7 @@
 {
   enable = true;
   defaultEditor = true;
-  extraLuaConfig = builtins.readFile ../config/nvim/init.lua;
+  initLua = builtins.readFile ../config/nvim/init.lua;
   plugins = with pkgs.vimPlugins; [
     catppuccin-nvim
   ];
